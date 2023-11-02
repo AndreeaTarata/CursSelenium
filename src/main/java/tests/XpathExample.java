60b43fd2868f71838c58e6ad3b4ce603f238106c
Andreea Tarata <andreeabtarata@gmail.com>
2023-11-02 21:35:04 +0200

    curs18 final

--- src/main/java/tests/XpathExample.java
+++ src/main/java/tests/XpathExample.java
@@ -3,12 +3,14 @@ package tests;
 import Utils.BaseTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class XpathExample extends BaseTest {
 	
 	
-	@Test
+	@Test(priority=1)
 	public void xpathExample() {
 		
 		driver.findElement(By.xpath("//li[@class='menu_user_login']")).click();
@@ -19,7 +21,34 @@ public class XpathExample extends BaseTest {
 		driver.findElement(By.xpath("//input[@class='submit_button']")).click();
 		
 	}
-	
+	@Test(priority=2)
+	public void xpathExample2() {
+		
+		driver.findElement(By.xpath("//span[@class='user_name']")).click();
+		//text() -> citeste textul dintre taguri <a href="https://keybooks.ro/wp-admin/profile.php" class="icon icon-cog">Settings</a>
+		driver.findElement(By.xpath("//a[text() = 'Settings']")).click();
+		
+		//contains
+		//referinta catre textul dintre taguri cu text()
+		driver.findElement(By.xpath("//a[contains(text(),'recent')]"));
+		//referinta catre atributul href cu @href
+		driver.findElement(By.xpath("//a[contains(@href , 'orders')]")).click();
+		
+		// direct child --> /
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		WebElement orderTbleHeader = driver.findElement(By.xpath("//th[contains(@class, 'woocommerce-orders-table__header')]/span[text()='Order']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red')", orderTbleHeader);
+		
+		// index
+		WebElement totalTabHaeder = driver.findElement(By.xpath("(//th[contains(@class, 'woocommerce-orders-table__header')]/span)[4]"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid orange')", totalTabHaeder);
+		
+		WebElement order = driver.findElement(By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell')][@data-title='Order']/a[not(contains(text(), '#172'))]"));
+		order.click();
+		
+		
+		
+	}
 	
 
 }
